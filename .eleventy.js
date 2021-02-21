acbd68b32cab743e7a16961e8a82a149d1979241
W. D. Porter <david.porter@fastmail.com>
2021-02-22 06:20:42 +1000

    initial commit

---
+++ .eleventy.js
@@ -0,0 +1,16 @@
+module.exports = config => {
+
+  config.addPassthroughCopy("src/META-INF")
+  config.addPassthroughCopy("src/acim.css")
+  config.addPassthroughCopy("src/metadata.opf")
+  config.addPassthroughCopy("src/mimetype")
+  config.addPassthroughCopy("src/toc.ncx")
+
+  return {
+
+    dir: {
+      input: "src",
+      output: "dist"
+    }
+  }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# New Testament
+
+## Knox Translation
+
+With paragraphing and section headings as per 1966 Jerusalem Bible
+
